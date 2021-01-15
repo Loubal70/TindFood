7e28da9c933d10e4fccf41b1ba66c2f04ad70d63
Loubal70 <louis.boulanger.work@gmail.com>
2021-01-15 12:12:12 +0100

    Maj travaux 15/01 12:12

---
+++ app/src/main/java/com/example/tindfood2/CategoriesAdapter.java
@@ -0,0 +1,21 @@
+package com.example.tindfood2;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoriesAdapter extends RecyclerView.Adapter{
+
+
+
+    class CategoryHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        ImageView image;
+        public CategoryHolder(View holder){
+            super(holder);
+            title = holder.findViewById(R.id.)
+        }
+    }
+}
