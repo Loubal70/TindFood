db29d139e0106937cd372513958c71b9ad2215c3
Loubal70 <louis.boulanger.work@gmail.com>
2021-01-18 17:15:27 +0100

    Maj travaux 18/01 17h15

---
+++ app/src/main/java/com/example/tindfood2/Adapter.java
@@ -0,0 +1,94 @@
+package com.example.tindfood2;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends  RecyclerView.Adapter<Adapter.FoodHolder>{
+
+    List<Item> data ;
+    int selectedItem = 0;
+
+    public Adapter(List<Item> data){
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public FoodHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.food_holder,parent, false);
+
+        return new FoodHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodHolder holder, int position) {
+        holder.price.setText(String.format("%d €", data.get(position).getPrice()));
+        holder.image.setImageResource(data.get(position).getImage());
+        holder.title.setText(data.get(position).getName());
+        holder.ratingBar.setRating(data.get(position).getRating());
+
+        if (selectedItem == position){
+            holder.cardView.animate().scaleX(1.1f);
+            holder.cardView.animate().scaleY(1.1f);
+            holder.title.setTextColor(Color.WHITE);
+            holder.price.setTextColor(Color.WHITE);
+            holder.item_bg.setBackgroundResource(R.drawable.splash);
+        }
+        else{
+            holder.cardView.animate().scaleX(1f);
+            holder.cardView.animate().scaleY(1f);
+            holder.title.setTextColor(Color.GRAY);
+            holder.price.setTextColor(Color.GRAY);
+            holder.item_bg.setBackgroundResource(R.color.white);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class FoodHolder extends RecyclerView.ViewHolder{
+        RatingBar ratingBar;
+        ImageView image;
+        TextView title;
+        TextView price;
+
+        LinearLayout item_bg;
+        CardView cardView;
+        public FoodHolder(View holder){
+            super(holder);
+
+            ratingBar = holder.findViewById(R.id.rating);
+            title = holder.findViewById(R.id.food_title);
+            image = holder.findViewById(R.id.food_img);
+            price = holder.findViewById(R.id.txt_price);
+            cardView = holder.findViewById(R.id.food_bg);
+            item_bg = holder.findViewById(R.id.item_bg);
+
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    selectedItem = getAdapterPosition();
+                    notifyDataSetChanged();
+                }
+            });
+
+        }
+    }
+}
--- app/src/main/java/com/example/tindfood2/MainActivity.java
+++ app/src/main/java/com/example/tindfood2/MainActivity.java
@@ -12,33 +12,81 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerCategories;
+    RecyclerView recyclerItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
+
             recyclerCategories = findViewById(R.id.recycler_categories);
+            recyclerItems = findViewById(R.id.recycler_nourriture);
             setCategories();
+            setItem(0);
     }
 
 
     private void setCategories() {
         List<Categories> data = new ArrayList<>();
 
-        Categories foodCategory = new Categories("Burger",R.drawable.ic_chicken_food);
-        Categories foodCategory2 = new Categories("Chicken",R.drawable.ic_chicken_food);
-        Categories foodCategory3 = new Categories("Pizza",R.drawable.ic_chicken_food);
-        Categories foodCategory4 = new Categories("Burger",R.drawable.ic_chicken_food);
+        Categories foodCategory = new Categories("Burger",R.drawable.ic_burger_food);
+        Categories foodCategory2 = new Categories("Poulet",R.drawable.ic_chicken_food);
+        Categories foodCategory3 = new Categories("Pizza",R.drawable.ic_pizza_food);
 
         data.add(foodCategory);
         data.add(foodCategory2);
         data.add(foodCategory3);
-        data.add(foodCategory4);
 
-        CategoriesAdapter CategoriesAdapter = new CategoriesAdapter(data, MainActivity.this);
+        CategoriesAdapter CategoriesAdapter = new CategoriesAdapter(data, MainActivity.this, new CategoriesAdapter.OnCategoryClick() {
+            @Override
+            public void onClick(int pos) {
+                setItem(pos);
+            }
+        });
 
         recyclerCategories.setLayoutManager(new LinearLayoutManager(MainActivity.this, RecyclerView.HORIZONTAL,false));
         recyclerCategories.setAdapter(CategoriesAdapter);
         CategoriesAdapter.notifyDataSetChanged();
     }
+
+    private void setItem(int pos){
+        List<Item> Item = new ArrayList<>();
+        switch (pos){
+            case 2:
+                Item Item1 = new Item("Pizza 1", 4.5f, 14, R.drawable.pizza_1);
+                Item Item2 = new Item("Pizza 2", 2f, 14, R.drawable.pizza_2);
+                Item Item3 = new Item("Pizza 3", 3.5f, 14, R.drawable.pizza_3);
+                Item Item4 = new Item("Pizza 4", 4.5f, 14, R.drawable.pizza_4);
+                Item Item5 = new Item("Pizza 5", 5f, 14, R.drawable.pizza_5);
+
+                Item.add(Item1);
+                Item.add(Item2);
+                Item.add(Item3);
+                Item.add(Item4);
+                Item.add(Item5);
+                break;
+            case 1:
+                Item Item6 = new Item("Poulet 1", 4.5f, 14, R.drawable.grill_chicken_1);
+                Item Item7 = new Item("Poulet 2", 2f, 14, R.drawable.grill_chicken_2);
+                Item Item8 = new Item("Poulet 3", 3.5f, 14, R.drawable.grill_chicken_3);
+
+                Item.add(Item6);
+                Item.add(Item7);
+                Item.add(Item8);
+                break;
+            case 0:
+                Item Item9 = new Item("Burger 1", 4.5f, 14, R.drawable.burger);
+                Item Item10 = new Item("Burger 2", 2f, 14, R.drawable.burger_two);
+
+                Item.add(Item9);
+                Item.add(Item10);
+                break;
+        }
+
+        Adapter Adapter = new Adapter(Item);
+        recyclerItems.setLayoutManager(new LinearLayoutManager(MainActivity.this, RecyclerView.HORIZONTAL, false));
+
+        recyclerItems.setAdapter(Adapter);
+    }
+
 }
